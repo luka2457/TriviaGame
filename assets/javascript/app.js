0ee286d81e03801017c099de55184be1d4be0c50
Luke Karlovich <luke.karlovich@colorado.edu>
2018-08-19 20:07:47 -0600

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,40 @@
-// question object
+// questions object
 var questions = [
     {
-        question: "what is the worlds longest river",
-        answers: ["Amazon", "Nile", "Yangtze", "Mississippi"],
-        answer: "Amazon"
+        question: 'Why is Michael given a gold medal at the end of "Office Olympics"?',
+        answers: ["for cruising in his Chrystler", "for being the flonkerton champion", "for closing on his condo", "for the most 'that's what she said' uses"],
+        answer: "for closing on his condo",
+        gifs: '<iframe src="https://giphy.com/embed/HSKfJVqTM6MOQ" width="480" height="249" frameBorder="0" class="giphy-embed" allowFullScreen style="margin:5%"></iframe><a href="https://giphy.com/gifs/the-office-HSKfJVqTM6MOQ"></a>'
     },
     {
-        question: "Which Country has the highest population density",
-        answers: ["Macau", "Monaco", "Singapore", "Hong Kong"],
-        answer: "Macau"
+        question: 'In the episode "Money", what themed room do Jim and Pam stay in at Schrute Farms?',
+        answers: ["America Room", "Irrigation Room", "Amish Tranquility", "Night Time"],
+        answer: "Irrigation Room",
+        gifs: '<iframe src="https://giphy.com/embed/pdAweEZArTA76" width="480" height="273" frameBorder="0" class="giphy-embed" allowFullScreen style="margin:5%"></iframe><a href="https://giphy.com/gifs/reaction-smile-the-office-pdAweEZArTA76"></a>'
     },
     {
-        question: "what is the largest dessert on Earth",
-        answers: ["Sahara", "Gobi", "Arabian", "Kalahari"],
-        answer: "Sahara"
+        question: "What was the drink Jim Halpert consistently bought from the breakroom's vending machine before returning to the Scranton branch from his brief time at the Stamford branch?",
+        answers: ["bottled water", "Coca-Cola", "peach iced tea", "grape soda"],
+        answer: "grape soda",
+        gifs: '<iframe src="https://giphy.com/embed/13jghlUIB6FHZm" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen style="margin:5%"></iframe><a href="https://giphy.com/gifs/13jghlUIB6FHZm"></a>'
     },
     {
-        question: "Which of these countries does <u>NOT</u> have a Monarch",
-        answers: ["Portugal", "Denmark", "Belgium", "Spain"],
-        answer: "Portugal"
+        question: "What movie does Michael say that Dwight cried during?",
+        answers: ["Love Story", "Pearl Harbor", "Titanic", "Armageddon"],
+        answer: "Armageddon",
+        gifs: '<iframe src="https://giphy.com/embed/5xtDarxOHIVfDcAoiqI" width="480" height="278" frameBorder="0" class="giphy-embed" allowFullScreen style="margin:5%"></iframe><a href="https://giphy.com/gifs/editingandlayout-the-office-cry-dwight-5xtDarxOHIVfDcAoiqI"></a>'
     },
     {
-        question: "What is the world's largest lake",
-        answers: ["Caspian Sea", "Lake Superior", "Lake Victoria", "Lake Huron"],
-        answer: "Caspian Sea"
+        question: "In Season 2: Episode 12 'The Injury', what is Michael's reasoning behind not staying home the day he burned his foot on a George Foreman grill?",
+        answers: ["He wanted his co-workers to worry and fuss over him.", "There was no toilet paper in his condominium.", "He wanted to be treated like it was a normal day.", "He did not want any special treatment."],
+        answer: "There was no toilet paper in his condominium.",
+        gifs: '<iframe src="https://giphy.com/embed/y1FoIhs0WnEkw" width="480" height="277" frameBorder="0" class="giphy-embed" allowFullScreen style="margin:5%"></iframe><a href="https://giphy.com/gifs/the-office-y1FoIhs0WnEkw"></a>'
+    },
+    {
+        question: "Which series regular was a member of a real life band called The Grass Roots?",
+        answers: ["Craig Robinson", "Creed Bratton", "Leslie David Baker", "Rainn Wilson"],
+        answer: "Creed Bratton",
+        gifs: '<iframe src="https://giphy.com/embed/ecGJ5mcN3kAZW" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen style="margin:5%"></iframe><a href="https://giphy.com/gifs/the-office-creed-bratton-a-benihana-christmas-ecGJ5mcN3kAZW"></a>'
     },
 ];
 
@@ -34,10 +45,10 @@ var wrongAnswer = 0;
 var timerId;
 var i = 0;
 
-//fun functions
+//functions
 $(document).ready(function () {
 
-    function startGame() {
+    function startGame() { //to start game
         i = 0;
         rightAnswer = 0;
         wrongAnswer = 0;
@@ -45,7 +56,7 @@ $(document).ready(function () {
         $("#wins").empty();
         $("#loses").empty();
         $("#answerDisplay").empty();
-        $("#startButton").html("<button>Start Game</button>");
+        $("#startButton").html('<button class="btn btn-outline-secondary btn-lg">Start Game</button>');
         $("#startButton").click(function () {
             $("#startButton").empty();
             timeStart();
@@ -53,14 +64,15 @@ $(document).ready(function () {
         });
     };
 
-    function timeStart() {
+    function timeStart() {  //to start timer
         timeLeft = 29;
         clearInterval(timerId);
         $("#timeRemaining").html("Time Remaining: 30 seconds");
         timerId = setInterval(function () {
             if (timeLeft == -1) {
                 viewResults(false);
-                //function to show answers
+                wrongAnswer++;
+                i++;
             } else {
                 $("#timeRemaining").html("Time Remaining: " + timeLeft + " seconds");
                 timeLeft--;
@@ -68,13 +80,14 @@ $(document).ready(function () {
         }, 1000);
     };
 
-    function nextQuestion() {
+    function nextQuestion() {  //to continue to next question
+        $("#gifsId").empty();
         $("#answerDisplay").empty();
         $("#questions").html(questions[i].question)
-        $("#answerChoice1").html("<button>" + questions[i].answers[0] + "</button>");
-        $("#answerChoice2").html("<button>" + questions[i].answers[1] + "</button>");
-        $("#answerChoice3").html("<button>" + questions[i].answers[2] + "</button>");
-        $("#answerChoice4").html("<button>" + questions[i].answers[3] + "</button>");
+        $("#answerChoice1").html('<button class="btn btn-outline-secondary w3-animate-left">' + questions[i].answers[0] + '</button>');
+        $("#answerChoice2").html('<button class="btn btn-outline-secondary w3-animate-right">' + questions[i].answers[1] + '</button>');
+        $("#answerChoice3").html('<button class="btn btn-outline-secondary w3-animate-left">' + questions[i].answers[2] + '</button>');
+        $("#answerChoice4").html('<button class="btn btn-outline-secondary w3-animate-right">' + questions[i].answers[3] + '</button>');
         $("button").click(function () {
             if (this.innerHTML === questions[i].answer) {
                 viewResults(true);
@@ -88,21 +101,21 @@ $(document).ready(function () {
         })
     }
 
-    function viewResults(answerGuessed) {
+    function viewResults(answerGuessed) { // to show answer after each question
 
         $("#answerChoice1").empty();
         $("#answerChoice2").empty();
         $("#answerChoice3").empty();
         $("#answerChoice4").empty();
+        $("#answerDisplay").html("Answer: " + questions[i].answer);
+        $("#gifsId").html(questions[i].gifs);
 
         clearInterval(timerId);
         if (answerGuessed === true) {
             $("#questions").html("Correct!");
-            $("#answerDisplay").html("Answer: " + questions[i].answer);
         }
         if (answerGuessed === false) {
             $("#questions").html("Incorrect!");
-            $("#answerDisplay").html("Answer: " + questions[i].answer);
         }
         resultTimer = setTimeout(function () {
             if (i == questions.length) {
@@ -114,13 +127,13 @@ $(document).ready(function () {
         }, 5000);
     }
 
-    function gameOver() {
+    function gameOver() {  //shows final score and allows player to play again
         $("#questions").empty();
         $("#timeRemaining").empty();
         $("#answerDisplay").html("Game Over");
         $("#wins").html("Answers Correct : " + rightAnswer);
         $("#loses").html("Answers Wrong : " + wrongAnswer);
-        $("#playAgain").html("<button>Play Again</button>");
+        $("#playAgain").html('<button class="btn btn-outline-secondary btn-lg">Play Again</button>');
         $("#playAgain").click(function () {
             startGame();
         });
@@ -128,6 +141,6 @@ $(document).ready(function () {
 
     }
 
-    startGame();
+    startGame(); // to display first start button 
 
 });
